7d223c89df041b87f95a26cbc311049aec5a0471
Nikola <king_hell@abv.bg>
2014-01-14 00:23:41 +0200

    Update Query-Unix.hpp

--- CPP/Query-Unix.hpp
+++ CPP/Query-Unix.hpp
@@ -1,3 +1,22 @@
+/*
+	C++ Unix GNU JC2-MP server query function by King_Hual
+	
+
+	Arguments:
+
+	string address - The IP address
+	int port [optional] - The port
+
+	Methods:
+
+	update() - Updates the information
+	isErroneous() - Returns true if any errors occured, false otherwise
+	getHostName() - Returns the hostname of a server
+	getPlayers() - Returns the amount of players currently connected to the server
+	getMaxPlayers() - Returns the maximum amount of players that can be connected to the server
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
